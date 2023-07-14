faf87ef78b91526798bc9a7ca6959dfc834a1f3c
Jahangir Alam Rocky <x00jahangir@gmail.com>
2023-07-14 12:57:52 +0600

    - removed generic code from views. - added detailed APIView based code in views.

--- api_assignment/dog_db_app/views.py
+++ api_assignment/dog_db_app/views.py
@@ -1,23 +1,73 @@
-from rest_framework import generics
-from .models import Dog, Breed
-from .serializers import DogSerializer, BreedSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Breed, Dog
+from .serializers import BreedSerializer, DogSerializer
+from rest_framework import status
 
 
-class DogList(generics.ListCreateAPIView):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+class BreedList(APIView):
+    def get(self, request):
+        breeds = Breed.objects.all()
+        serializer = BreedSerializer(breeds, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = BreedSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-class DogDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+class BreedDetail(APIView):
+    def get(self, request, pk):
+        breed = Breed.objects.get(pk=pk)
+        serializer = BreedSerializer(breed)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        breed = Breed.objects.get(pk=pk)
+        serializer = BreedSerializer(breed, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        breed = Breed.objects.get(pk=pk)
+        breed.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class BreedList(generics.ListCreateAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
+class DogList(APIView):
+    def get(self, request):
+        dogs = Dog.objects.all()
+        serializer = DogSerializer(dogs, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-class BreedDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Breed.objects.all()
-    serializer_class = BreedSerializer
+class DogDetail(APIView):
+    def get(self, request, pk):
+        dog = Dog.objects.get(pk=pk)
+        serializer = DogSerializer(dog)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        dog = Dog.objects.get(pk=pk)
+        serializer = DogSerializer(dog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        dog = Dog.objects.get(pk=pk)
+        dog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
